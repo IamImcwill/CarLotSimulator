7d44e66703e4a9dd3f9926134494d6c49d75f269
Caris Williams <cwill1013@gmail.com>
2024-01-24 22:33:26 -0500

    Inital Commit

---
+++ CarLotSimulatorApp/CarLotSimulator/Cars.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    internal class Car
+    {
+        public Car() { }
+
+        public Car(string make, string model, int year, string engineNoise, string honkNoise, bool isDrivable) 
+        
+        {
+            Make = make;
+            Model = model;  
+            Year = year;    
+            EngineNoise = engineNoise;  
+            HonkNoise = honkNoise;
+            IsDriveble = isDrivable;
+        }
+
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveble { get; set; }
+
+        public void MakeEgineNoise(string noise)
+        {
+            Console.WriteLine($"The {Make}'s engine noise goes {noise}");
+        }
+        public void MakeHonkNoise(string noise)
+            {
+                Console.WriteLine($"The {Make}'s honk noise goes {HonkNoise}");
+            }
+
+
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -8,16 +8,48 @@ namespace CarLotSimulator
         {
             //TODO
 
-            //Create a seperate class file called Car
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
+            //Create a seperate class file called Car DONE!!!!!!!
+            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable DONE!!!!!
+            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise() DONE!!!!!
+            //The methods should take one string parameter: the respective noise property DONE!!!!!!
 
 
-            //Now that the Car class is created we can instanciate 3 new cars
-            //Set the properties for each of the cars
-            //Call each of the methods for each car
+            //Now that the Car class is created we can instanciate 3 new cars DONE!!!!
+            //Set the properties for each of the cars DONE!!!!!!!
+            //Call each of the methods for each car Done!!!!!
 
+            CarLot instance = new CarLot();
+            
+
+            Car car1 = new Car(); 
+            car1.Make = "Acrua";
+            car1.Model = "MDX Type S";
+            car1.Year = 2025;
+            car1.EngineNoise = "VROOOM";
+            car1.HonkNoise = "beep beep";
+            car1.IsDriveble = true;
+            instance.ListOfCars.Add(car1);
+
+            Car car2 = new Car() {Make = "Lexus", Model = "RX 350 F Sport", EngineNoise = "VROOOOM", HonkNoise = "beep beep", Year = 2025, IsDriveble = true}; 
+            
+
+            Car car3 = new Car("Mercedes-Benz", "AMG GLE 63 S Coupe", 2025, "VROOOOOOOOM", "beep beep", true);
+
+            instance.ListOfCars.Add(car1);
+            instance.ListOfCars.Add(car2);
+            instance.ListOfCars.Add(car3);
+
+            foreach (Car item in instance.ListOfCars) 
+            { 
+                Console.WriteLine($"{item.Make}");
+                Console.WriteLine($"{item.Model}");
+                Console.WriteLine($"{item.Year}");
+                Console.WriteLine($"{item.EngineNoise}");
+                Console.WriteLine($"{item.HonkNoise}");
+                Console.WriteLine($"{item.IsDriveble}");
+                Console.WriteLine();
+                Console.WriteLine();
+            }
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
